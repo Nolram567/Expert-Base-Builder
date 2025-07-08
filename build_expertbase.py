318ae35f7043c526cb89684c5d49e680b08aff70
Nolram567 <74004417+Nolram567@users.noreply.github.com>
2025-07-08 10:20:16 +0200

    feat: add method use of add_properties_from_csv

--- build_expertbase.py
+++ build_expertbase.py
@@ -15,11 +15,13 @@ console_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s
 console_handler.setFormatter(console_formatter)
 logger.addHandler(console_handler)
 
-def main(csv_file: str, output_qmd: str, output_yml: str) -> None:
+def main(csv_file: str, csv_extension: str, output_qmd: str, output_yml: str) -> None:
     try:
         logger.info(f"Starte die Verarbeitung der Expert Base mit der Datei: {csv_file}")
 
         expert_base = ExpertBase(csv_file, from_csv=True)
+
+        expert_base.add_properties_from_csv(csv_extension)
 
         # Serialisiere die Expert Base als JSON für die Begutachtung.
         expert_base.serialize_expert_base("Expert-Base-Builder/backup.json")
@@ -39,6 +41,7 @@ if __name__ == "__main__":
 
     main(
         csv_file="Expert-Base-Builder/data/orcids.csv",
+        csv_extension="config/property_extension.csv",
         output_qmd = "Expert-Base-Builder/outputs/experts",
         output_yml = "Expert-Base-Builder/outputs/",
     )
