0840f749b0518b1f78d61db923f7b666b3af46d5
Nolram567 <74004417+Nolram567@users.noreply.github.com>
2025-08-20 10:33:08 +0200

    feat: revise exception handling

--- expert_base_builder/expert_base.py
+++ expert_base_builder/expert_base.py
@@ -130,6 +130,9 @@ class ExpertBase:
 
         Args:
             path: Der Dateipfad, unter dem das Expertbase-Objekt abgespeichert werden soll.
+
+        Raises:
+            IOError: Wenn die Datei unter dem spzifizierten Pfad nicht gefunden wurde.
         """
         try:
             with open(path, "r", encoding='utf-8') as f:
@@ -151,8 +154,8 @@ class ExpertBase:
             logger.info(f"Das Expertbase-Objekt wurde erfolgreich von {path} eingelesen.")
 
         except IOError as e:
-
-            logger.error(f"Fehler beim Deserialisieren der Expertbase unter {path}:\n{e}")
+            logger.error(f"Fehler beim Deserialisieren der Expert Base unter {path}:\n{e}")
+            raise
 
     def serialize_expert_base(self, path: str, name: str) -> None:
         """
@@ -226,6 +229,9 @@ class ExpertBase:
 
         Args:
              path: Der Dateipfad zu der CSV-Datei.
+
+        Raises:
+            IOError: Wenn die CSV-Datei nicht gefunden wurde.
         """
 
         try:
@@ -235,7 +241,7 @@ class ExpertBase:
             orcids = self.get_orcids_as_list()
         except IOError as e:
             logger.error(f"Die Datei {path} konnte nicht geöffnet werden:\n {e}")
-            return
+            raise
 
         if len(properties) < 2:
             logger.warning(f"Die CSV-Datei ist ungültig. Es muss mindestens 3 Spalten geben. Abbruch...")
